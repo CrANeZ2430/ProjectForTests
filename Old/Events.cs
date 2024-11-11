b3084d6546aef05edd68e0506b9daeccb4044b9b
CrnZ1675 <crn.alexei@gmail.com>
2024-11-11 23:56:09 +0200

    Added old files

---
+++ Old/Events.cs
@@ -0,0 +1,44 @@
+﻿class Events
+{
+    //public delegate void EventDelegate(object sender, EventArgs e);
+    //public EventDelegate OnHappenedWithSomeone;
+
+    //action event
+    //public event Action<string> Event;
+    
+    //function event
+    //public event Func<string, bool> Event;
+
+    public event EventHandler<OnHappenedArgs>? OnHappened;
+
+    public class OnHappenedArgs : EventArgs
+    {
+        public readonly int argsNum;
+
+        public OnHappenedArgs(int constructorNum)
+        {
+            argsNum = constructorNum;
+        }
+    }
+
+    private readonly int NumVariable = 10;
+
+    public void Method()
+    {
+        //Event += EventMethod;
+        OnHappened += GeneralOnHappened;
+
+        //Event?.Invoke("Hello World!");
+        OnHappened?.Invoke(this, new OnHappenedArgs(NumVariable) /* { var = var } */);
+    }
+
+    private void GeneralOnHappened(object? sender, OnHappenedArgs e)
+    {
+        Console.WriteLine("Your variable is " + e.argsNum);
+    }
+
+    /*private void EventMethod(string obj)
+    {
+        Console.WriteLine(obj);
+    }*/
+}
