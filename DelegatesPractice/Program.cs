eed5babed05b05d724aab96ed92e21a5937c4f98
CrnZ1675 <crn.alexei@gmail.com>
2024-11-17 01:36:49 +0200

    .net 9 and added delegate test file

---
+++ DelegatesPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// DoSomethingWithData(x =>
+// {
+//     Console.WriteLine(x);
+// });
+//
+// void DoSomethingWithData(Action<int> callback)
+// {
+//     callback(42);
+// }
+
