503279fc28b8c6d58219e214f2c2f129124672cf
CrnZ1675 <crn.alexei@gmail.com>
2024-11-17 19:21:35 +0200

    Delegate tasks

---
+++ DelegateTask1/Program.cs
@@ -0,0 +1,29 @@
+﻿var op = new OrderProcessor(123);
+
+op.OrderCompleted += orderNumber =>
+{
+    Console.WriteLine($"Order completed: Order #{orderNumber} \nNotification: Your order #{orderNumber} is now complete!");
+};
+
+op.MakeOrder();
+
+internal class OrderProcessor
+{
+    internal delegate void OrderHandler(byte orderNumber);
+    
+    public event OrderHandler? OrderCompleted;
+
+    private byte OrderNumber { get; }
+
+    internal OrderProcessor(byte orderNumber)
+    {
+        OrderNumber = orderNumber;
+    }
+
+    public void MakeOrder()
+    {
+        Console.WriteLine($"Order placed: Order #{OrderNumber}");
+        Console.WriteLine($"Processing order: Order #{OrderNumber}");
+        OrderCompleted?.Invoke(OrderNumber);
+    }
+}
---
+++ DelegateTask2/Program.cs
@@ -0,0 +1,43 @@
+﻿var stock = new Stock("APPL");
+
+stock.StockChanged += (price, stockName) =>
+{
+    Console.WriteLine($"Stock: {stockName}, New Price: ${price}");
+    if (price >= 160)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Alert: {stockName} stock price exceeded $160.00!");
+        Console.ResetColor();
+    }
+};
+
+while (stock.Price < 160)
+{
+    stock.DisplayPrice();
+}
+
+
+
+class Stock
+{
+    private string StockName { get; }
+    public decimal Price { get; private set; }
+    public event Action<decimal, string> StockChanged;
+
+    public Stock(string stockName)
+    {
+        StockName = stockName;
+    }
+
+    private void ChangePrice()
+    {
+        Price = Random.Shared.Next(200);
+    }
+
+    public void DisplayPrice()
+    {
+        ChangePrice();
+        StockChanged?.Invoke(Price, StockName);
+        Thread.Sleep(2000);
+    }
+}
