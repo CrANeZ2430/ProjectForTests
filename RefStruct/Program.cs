12dac97a49f17819c02b9ef3cc213c4fea1c60e6
CrnZ1675 <crn.alexei@gmail.com>
2024-12-14 19:38:08 +0200

    Struct test

---
+++ RefStruct/Program.cs
@@ -0,0 +1,14 @@
+﻿MyStruct myStruct = new MyStruct(12, 156);
+Console.WriteLine($"Age: {myStruct.Age} y.o., height: {myStruct.Height} sm");
+
+public class School
+{
+    // an error
+    //static MyStruct myStruct = new MyStruct();
+}
+
+public ref struct MyStruct(int age, int name)
+{
+    public int Age { get; set; } = age;
+    public int Height { get; set; } = name;
+}
